3ce46fdd19678917b2e2d3685c831c73b9104933
Wei Junyu <weijunyu@users.noreply.github.com>
2020-12-03 22:25:56 +0800

    Clean up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -67,7 +67,7 @@ function App() {
         <StyledHeader>
           <h1 style={{ textAlign: "center" }}>
             <StyledHeaderLink exact to="/">
-              <i className="fas fa-key" /> Secrits!
+              <i className="fas fa-key" /> Secrets!
             </StyledHeaderLink>
           </h1>
           <StyledNav>
