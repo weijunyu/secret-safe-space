0b65f65f59c4ed141797f54bc0d0dfccc0a779c0
C4BEWGuSO9A <C4BEWGuSO9A@users.noreply.github.com>
2020-12-17 00:51:38 +0800

    Proper deletion of expired ones

--- functions/src/scheduled/cleanUp.ts
+++ functions/src/scheduled/cleanUp.ts
@@ -1,5 +1,6 @@
 import { firebaseAdmin, firestore } from "../lib/firebase";
 import { SECRET_PATH_COLLECTION } from "../lib/constants";
+
 export async function removeExpiredSecrets(): Promise<void> {
   const expiredDocs = await firestore
     .collection(SECRET_PATH_COLLECTION)
@@ -9,7 +10,15 @@ export async function removeExpiredSecrets(): Promise<void> {
       firebaseAdmin.firestore.Timestamp.fromDate(new Date())
     )
     .get();
-  expiredDocs.forEach((doc) => {
-    console.log(doc.data());
-  });
+
+  if (!expiredDocs.empty) {
+    const batch = firestore.batch();
+    expiredDocs.forEach((doc) => {
+      console.log(`Deleting expired doc: ${doc.id}`);
+      batch.delete(firestore.collection(SECRET_PATH_COLLECTION).doc(doc.id));
+    });
+    await batch.commit();
+  } else {
+    console.log("No expired entries were deleted.");
+  }
 }
