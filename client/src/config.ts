c906bd900fd1e5c2cfc089bb431261816f55b7fc
C4BEWGuSO9A <C4BEWGuSO9A@users.noreply.github.com>
2020-12-17 12:30:53 +0800

    Adding deployment env var

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,3 +2,4 @@ export const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 export const firebaseConfig = JSON.parse(
   process.env.REACT_APP_FIREBASE_CONFIG || "{}"
 );
+export const deploymentEnv = process.env.REACT_APP_DEPLOYMENT_ENV;
