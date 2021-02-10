572c596098dea151eb0fe1e10ded98a0c8458283
Wei Junyu <weijunyu@users.noreply.github.com>
2021-02-11 02:43:48 +0800

    File reading into b64

--- client/src/components/SecretFileInput.tsx
+++ client/src/components/SecretFileInput.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Uppy, { UppyFile } from "@uppy/core";
+import DragDrop from "@uppy/drag-drop";
+import styled from "styled-components";
+import { encode } from "base64-arraybuffer";
+
+import "@uppy/core/dist/style.css";
+import "@uppy/drag-drop/dist/style.css";
+
+const StyledSecretFileInput = styled.div`
+  padding-bottom: 1rem;
+`;
+
+const fileDragDropEl = "secret-file-input";
 
 export default function SecretFileInput() {
-  return <div>secret file input</div>;
+  useEffect(() => {
+    Uppy({
+      onBeforeFileAdded: function (currentFile: UppyFile) {
+        currentFile.data.arrayBuffer().then((buf) => console.log(encode(buf)));
+        return false;
+      },
+    }).use(DragDrop, { target: "#" + fileDragDropEl });
+  }, []);
+  return <StyledSecretFileInput id={fileDragDropEl}></StyledSecretFileInput>;
 }
